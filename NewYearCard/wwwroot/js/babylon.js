03eba62b0867989cc9b8ebe8028f4bbb3a2d9fb8
PeeAre <silencetray@gmail.com>
2023-12-03 00:27:58 +0100

    Refactored babylon.js

--- NewYearCard/wwwroot/js/babylon.js
+++ NewYearCard/wwwroot/js/babylon.js
@@ -5,7 +5,19 @@ function loadBabylon(canvasId) {
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
 
-    BABYLON.SceneLoader.ImportMesh("", "", "https://dl.dropboxusercontent.com/scl/fi/dx5m9e3ngq61ueylzf38u/TEST1.glb?rlkey=6uow6y7v8iktapb1bru4vhtey&dl=0", scene);
+    BABYLON.SceneLoader.ImportMeshAsync("", "",
+        "https://dl.dropboxusercontent.com/scl/fi/0jbcidmpg6gjp1l6kxuuc/TEST5.glb?rlkey=yw5fpbmyzrifvzctybstws94m&dl=0",
+        scene)
+        .then((result) => {
+            var root = result.meshes;
+            root.forEach((element) => element.rotate(BABYLON.Vector3.Up(), Math.PI));
+            result.animationGroups.forEach((element) => {
+                if (element.name === 'toolPickaxeAction') {
+                    element.start(true);
+                }
+            });
+            console.log({ result });
+        });
 
     var camera = new BABYLON.FreeCamera("camera1",
         new BABYLON.Vector3(0, 5, -10),
@@ -14,12 +26,12 @@ function loadBabylon(canvasId) {
     camera.setTarget(BABYLON.Vector3.Zero());
     // Attaches the camera to the canvas
     camera.attachControl(canvas, true);
-    // Creates a light, aiming 0,1,0
-    var light = new BABYLON.HemisphericLight("light",
-        new BABYLON.Vector3(0, 1, 0),
-        scene);
-    // Dim the light a small amount 0 - 1
-    light.intensity = 1.8;
+    //// Creates a light, aiming 0,1,0
+    //var light = new BABYLON.HemisphericLight("light",
+    //    new BABYLON.Vector3(0, 1, 0),
+    //    scene);
+    //// Dim the light a small amount 0 - 1
+    //light.intensity = 0.5;
     engine.runRenderLoop(function () {
         scene.render();
     });
@@ -34,7 +46,7 @@ function createScene(canvas, engine) {
     // Creates a basic Babylon Scene object
     var scene = new BABYLON.Scene(engine);
     // Creates and positions a free camera
-    
+
     // Built-in 'sphere' shape.
     var sphere = BABYLON.MeshBuilder.CreateSphere("sphere",
         { diameter: 2, segments: 32 },
